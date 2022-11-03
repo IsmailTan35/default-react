10ff0b8e50dce1fc8d912d356c72c63924e4d4c3
IsmailTan35 <tan_tan.2000@hotmail.com>
2022-11-03 21:36:27 +0300

    firtst commit

---
+++ src/component/Context.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+// import { io } from "socket.io-client";
+// const host = window.location.hostname;
+// const protocol = window.location.protocol;
+// export const url =
+//   host == "otosmart.vercel.app"
+//     ? process.env.REACT_APP_URL_PRODUCTION
+//     : `${protocol}//${host}:11000`;
+// export const client = io.connect(
+//   url.replace("http://", "").replace("https://", "").replace("/", ""),
+//   {
+//     reconnection: true,
+//     reconnectionDelay: 2500,
+//     reconnectionAttempts: 10,
+//   }
+// );
+// export const SocketContext = React.createContext();
---
+++ src/component/SocketController.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SocketController = () => {
+  return null;
+};
+
+export default SocketController;
